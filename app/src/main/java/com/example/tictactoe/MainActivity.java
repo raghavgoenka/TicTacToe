99126fd6c0d9ebbe1246ad945eb4da0d419c22f3
raghavgoenka <raghavgoenka33@gmail.com>
2021-01-12 13:25:43 +0530

    Added theme

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.DialogFragment;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.GridLayout;
@@ -16,32 +19,39 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     GridLayout grid;
+    TextView turn;
     int activep = 0;
     boolean gameactive = true;
     int[] gamestate = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winPos = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
-    // = new AlertDialog.Builder(this);
+
+
 
 
     public void dropIn(View view) {
-
+        turn=(TextView) findViewById(R.id.turn);
         ImageView counter = (ImageView) view;
         counter.getTag();
         int tappedcounter = Integer.parseInt(counter.getTag().toString());
 
-        //counter.setTranslationY(-1000f);
-        //counter.setImageResource(R.drawable.yellow);
-        //
+        turn.setText("Yellow's Turn Tap to play!");
+
         if (gamestate[tappedcounter] == 2 && gameactive) {
             gamestate[tappedcounter] = activep;
             counter.setTranslationY(-1000f);
             if (activep == 0) {
                 counter.setImageResource(R.drawable.yellow);
                 activep = 1;
+
+                turn.setText("Red's Turn Tap to play!");
+                turn.setTextColor(Color.parseColor("#F11111"));
             } else {
                 counter.setImageResource(R.drawable.red);
                 activep = 0;
+
+                turn.setText("Yellow's Turn Tap to play!");
+                turn.setTextColor(Color.parseColor("#F9A825"));
             }
             counter.animate().translationYBy(1000f).rotation(720).setDuration(50);
             for (int[] winPos : winPos) {
@@ -52,42 +62,10 @@ public class MainActivity extends AppCompatActivity {
                         winner = "Yellow";
                     }
                     {
-                        //someone has won
+
                         TextView winnermessage = (TextView) findViewById(R.id.winner);
                         winnermessage.setText(winner + " has won" + " Wohoo !");
 
-//                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-//                        alertDialogBuilder.setMessage(winner+" has won!!!\n DO you want to play again? ");
-//                        alertDialogBuilder.setPositiveButton("yes",
-//                                new DialogInterface.OnClickListener() {
-//                                    @Override
-//                                    public void onClick(DialogInterface arg0, int arg1) {
-//
-//                                        gameactive=true;
-//                                        activep=0;
-//
-//                                        for(int i=0;i<gamestate.length;i++){
-//                                            gamestate[i]=2;
-//                                        }
-//                                        {
-//                                            for(int  i=0;i<grid.getChildCount();i++)
-//                                            {
-//
-//                                                ((ImageView)grid.getChildAt(i)).setImageResource(0);
-//                                            }}
-//                                        Toast.makeText(MainActivity.this, "You clicked yes button", Toast.LENGTH_LONG).show();
-//                                    }
-//                                });
-//
-//                        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                finish();
-//                            }
-//                        });
-//
-//                        AlertDialog alertDialog = alertDialogBuilder.create();
-//                        alertDialog.show();
                         LinearLayout layout = findViewById(R.id.playagainlayout);
                         layout.setVisibility(View.VISIBLE);
                     }
@@ -98,38 +76,7 @@ public class MainActivity extends AppCompatActivity {
                             gameIsover = false;
                     }
                     if (gameIsover) {
-//                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-//                        alertDialogBuilder.setMessage("It is a Draw! Do you want to play again?");
-//                        alertDialogBuilder.setPositiveButton("yes",
-//                                new DialogInterface.OnClickListener() {
-//                                    @Override
-//                                    public void onClick(DialogInterface arg0, int arg1) {
-//
-//                                        gameactive=true;
-//                                        activep=0;
-//
-//                                        for(int i=0;i<gamestate.length;i++){
-//                                            gamestate[i]=2;
-//                                        }
-//
-//                                        for (int i = 0; i < grid.getChildCount(); i++) {
-//                                            ((ImageView) grid.getChildAt(i)).setImageResource(0);
-//                                        }
-//
-//                                        Toast.makeText(MainActivity.this, "You clicked yes button", Toast.LENGTH_LONG).show();
-//                                    }
-//                                });
-//
-//                        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                finish();
-//                            }
-//                        });
-//
-//                        AlertDialog alertDialog = alertDialogBuilder.create();
-//                        alertDialog.show();
-//                    }
+
                         TextView winnermessage = (TextView) findViewById(R.id.winner);
                         winnermessage.setText("It's a draw!");
                         LinearLayout layout = findViewById(R.id.playagainlayout);
@@ -145,10 +92,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         grid = findViewById(R.id.gridlayout);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
+
 
     public void playAgain(View v)
     {
+       turn.setText("Yellow's turn Tap to play!");
+        turn.setTextColor(Color.parseColor("#F9A825"));
         gameactive=true;
         activep=0;
 
